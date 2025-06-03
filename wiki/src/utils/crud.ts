fdc353ea728deffd7c15fc49a323707e6498a90d
lharrison1910 <lewis.harrison0806@gmail.com>
2025-06-03 15:12:20 +0100

    crud operations ad

--- wiki/src/utils/crud.ts
+++ wiki/src/utils/crud.ts
@@ -1,6 +1,16 @@
+import PocketBase from "pocketbase";
+
+const pb = new PocketBase("http://192.168.1.3:8089");
+
 export function Create() {}
 
-export function Delete() {}
+export function Delete(id: string) {
+  try {
+    pb.collection("Wiki").delete(id);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export function Update() {}
 
