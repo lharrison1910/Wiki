b7c7ea929d38690f99f99d26195af7946a8136d2
lharrison1910 <lewis.harrison0806@gmail.com>
2025-06-04 08:34:32 +0100

    updated crud

--- wiki/src/utils/crud.ts
+++ wiki/src/utils/crud.ts
@@ -1,8 +1,15 @@
 import PocketBase from "pocketbase";
+import type { FileProps } from "../types/FileType";
 
 const pb = new PocketBase("http://192.168.1.3:8089");
 
-export function Create() {}
+export function Create(data: FileProps) {
+  try {
+    pb.collection("Wiki").create(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export function Delete(id: string) {
   try {
@@ -12,6 +19,12 @@ export function Delete(id: string) {
   }
 }
 
-export function Update() {}
+export function Update(data: FileProps) {
+  try {
+    pb.collection("Wiki").update(data.id, data);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export function Read() {}
