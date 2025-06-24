58ffb985256d7248b05f280ec0b2efcb1c4a75e4
lharrison1910 <lewis.harrison0806@gmail.com>
2025-06-24 12:50:06 +0100

    made a good start on the AI ui and some other minor changes

--- backend/server.py
+++ backend/server.py
@@ -112,4 +112,4 @@ def fetchData():
 
 
 if "__main__" == __name__:
-    app.run(debug=True)
+    app.run(debug=True, port=5050)
--- wiki/src/pages/homepage/Homepage.tsx
+++ wiki/src/pages/homepage/Homepage.tsx
@@ -5,6 +5,7 @@ import ListView from "../../components/ListView/ListView";
 import CardView from "../../components/CardView/CardView";
 import { AttachFile } from "@mui/icons-material";
 import { useData } from "../../context/dataContext";
+import AI from "../../components/AIPopup/AI";
 
 function Homepage(props: { display: string | undefined }) {
   const {
@@ -99,6 +100,7 @@ function Homepage(props: { display: string | undefined }) {
           />
         </Button>
       </div>
+      <AI />
 
       {/*feedback */}
       <div>
